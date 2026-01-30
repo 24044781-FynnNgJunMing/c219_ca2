f05abf8720a62cec1ed45d696fca3c46cdb97f2c
Fikri <mohammed.danish.fikri@gmail.com>
2026-01-30 23:27:41 +0800

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.REACT_APP_API_URL ||
-  "https://onlinestudyspaceswebservice.onrender.com/";
+  "https://onlinestudyspaceswebservice.onrender.com";
 
 function authHeader() {
   const token = localStorage.getItem("token");
